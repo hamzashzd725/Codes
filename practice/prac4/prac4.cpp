14e9138a01eeede3993e5dd3921806420572ed74
Hamza Shahzad <hamzashzd725@gmail.com>
2026-02-20 18:30:05 +0500

    Adding task4

--- practice/prac4/prac4.cpp
+++ practice/prac4/prac4.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
-    
+    int height;
+    do {
+        cout<<"Height: ";
+        cin>>height;
+    } while(height < 0);
+    for (int i = 0; i < height; i++) {
+        for (int j  = 0; j <= i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
 }
